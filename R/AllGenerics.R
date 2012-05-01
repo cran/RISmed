c8590d86da84816707a701cad628615fdfb2caf8
S. A. Kovalchik <s.a.kovalchik@gmail.com>
2012-05-01 00:00:00 +0000

    version 2.0

---
+++ R/AllGenerics.R
@@ -0,0 +1,4 @@
+# RISmed GENERICS
+setGeneric("count",function(object) standardGeneric("count"))
+setGeneric("id",function(object) standardGeneric("id"))
+setGeneric("translation",function(object) standardGeneric("translation"))
